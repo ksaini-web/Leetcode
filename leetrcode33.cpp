842f42bfe8faded04b00f88feed3d215b2a86fff
ksaini-web <kartiksaini9084@gmail.com>
2025-11-22 19:17:12 +0530

    Implement binary search in rotated sorted array

---
+++ leetrcode33.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int tar) {
+
+        int st = 0, end = nums.size() - 1;
+
+        while (st <= end) {
+
+            int mid = st + (end - st) / 2;
+
+            if (nums[mid] == tar) {
+                return mid;
+            }
+
+            // Left half sorted
+            if (nums[st] <= nums[mid]) {
+
+                if (nums[st] <= tar && tar < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
+                }
+
+            }
+            // Right half sorted
+            else {
+
+                if (nums[mid] < tar && tar <= nums[end]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+
+            }
+        }
+
+        return -1;
+    }
+};
