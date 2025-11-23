81f3cd0a10dc9301c35aa62b3771caafa4a0a8a1
ksaini-web <kartiksaini9084@gmail.com>
2025-11-23 18:26:03 +0530

    Add maxProduct function in leetcode152.cpp
    Implement maxProduct function to find maximum product of subarray.

---
+++ leetcode152.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+
+ int currmax=nums[0];
+  int currmin=nums[0];
+
+  int ans=nums[0];
+
+  for(int i=1;i<nums.size();i++){
+
+    if(nums[i]<0){
+        swap(currmax,currmin);
+        
+    }
+
+
+currmax=max(nums[i],currmax*nums[i]);
+currmin=min(nums[i],currmin*nums[i]);
+
+ans=max(ans,currmax);
+  }
+
+    return ans;
+
+        
+    }
+};
