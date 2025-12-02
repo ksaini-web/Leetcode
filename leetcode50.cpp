0b31a77a03da68a1e1e8d3b5f0be55441de8bf09
ksaini-web <kartiksaini9084@gmail.com>
2025-12-02 18:43:36 +0530

    Implement myPow function for power calculation

---
+++ leetcode50.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0){return 1.0;}
+        if(x==0){return 0;}
+        if(x==1){return 1.0;}
+        if(x==-1&&n%2==0){return 1.0; }
+        if(x==-1&&n%2!=0){return -1.0;}
+
+        long binary=n;
+
+        if(n<0){
+            x=1/x;
+            binary=-binary;
+        }
+
+        double ans=1;
+
+        while(binary>0){
+            if(binary%2==1){
+                ans*=x;
+            }
+            x*=x;
+            binary/=2;}
+
+        
+        return ans;
+        
+    }
+};
