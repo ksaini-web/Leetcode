068841da42f4c3b742ffc5ba4614c9d2db82896f
ksaini-web <kartiksaini9084@gmail.com>
2025-12-06 19:10:50 +0530

    Add isPalindrome function using isalnum
    Implement palindrome checking with alphanumeric filtering.

---
+++ leetcode125using_isalnum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    //   bool isAlhaNumerie(char ch){
+
+    // if(ch>='0'&&ch<='9'||tolower(ch)>='a'&&tolower(ch)<='z'){
+    //     return true;
+    // }return false;
+    //   }
+    bool isPalindrome(string s) {
+
+        int st=0,end=s.size()-1;
+
+        while(st<end){
+
+            if(!isalnum(s[st])){
+                st++;
+                continue;}
+
+                 if(!isalnum(s[end])){
+                end--;
+                continue;}
+
+                if(tolower(s[st])!=tolower(s[end])){
+                    return false;
+                }
+                st++;end--;
+        }
+        return true;
+        
+    }
+};
