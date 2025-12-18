817a78655a9ee8243dc65d01b5511c726fc952ac
ksaini-web <kartiksaini9084@gmail.com>
2025-12-18 18:28:05 +0530

    Implement solution for counting permutations

---
+++ leetcode3577.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    static const long long MOD = 1e9 + 7;
+    long long factorial(long long n) {
+        long long res = 1;
+        for (long long i = 1; i <= n; i++)
+            res = (res * i) % MOD;
+        return res;
+    }
+    int countPermutations(vector<int>& complexity) {
+        int n = complexity.size();
+        long long mn = complexity[0];
+        for (int i = 1; i < n; i++) {
+            if (mn >= complexity[i]) {
+                return 0;
+            }
+            mn = min(mn, (long long)complexity[i]);
+        }
+        return factorial(n - 1);
+    }
+};
