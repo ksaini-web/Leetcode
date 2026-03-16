579aab1cd98727d6f1ce64f949484d075013e2a6
ksaini-web <kartiksaini9084@gmail.com>
2026-03-16 15:04:42 +0530

    Add function to convert sorted array to BST
    Implement a function to convert a sorted array to a height-balanced BST.

---
+++ leetcode108.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+TreeNode* bulid(vector<int>&nums,int left ,int right){
+
+    if(left>right){
+        return NULL;
+    }
+
+    int mid = (left+right)/2;
+
+    TreeNode* root  = new TreeNode(nums[mid]);
+
+   root->left = bulid(nums,left,mid-1);
+
+   root->right = bulid(nums,mid+1,right);
+
+   return root;
+
+
+
+
+}
+
+
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+
+        return bulid(nums,0,nums.size()-1);
+
+    
+        
+    }
+};
