bda2ecbf2b65d4c76138c4acec57aff295a3d1a4
ksaini-web <kartiksaini9084@gmail.com>
2026-04-01 19:18:59 +0530

    Implement BST construction from preorder traversal

---
+++ leetcode1002.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:  
+
+int i =0;
+
+TreeNode* build(vector<int>& preorder , int upperBound){
+
+    if(i== preorder.size() ||preorder[i]>upperBound ){
+        return NULL;
+    }
+
+
+    TreeNode* root =new TreeNode(preorder[i]);
+    i++;
+
+    root->left = build(preorder,root->val);
+    root->right = build(preorder,upperBound);
+
+
+    return root;
+
+
+}
+               
+            
+    TreeNode* bstFromPreorder(vector<int>& preorder) { 
+
+        return build(preorder,INT_MAX);
+        
+    }
+};
